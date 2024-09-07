c67f8c64fd2d20843b5c1b8dc3ca029cd697acb2
Victor David Santos <victordavidsantos@gmail.com>
2024-09-06 21:24:48 -0300

    Placing exits (in progress)

--- game/app/editor.rb
+++ game/app/editor.rb
@@ -1,5 +1,7 @@
 require_relative 'scene'
 
+$font = Font.new(:font, 32)
+
 class EditorScene < Scene
   RENDER_TARGET_ID = :editor_scene
   DISPLAY_WIDTH = 640
@@ -39,11 +41,16 @@ class EditorScene < Scene
     end
 
     @entrances.each { |(i, j)| Window.draw_rect(i * TILE_SIZE, j * TILE_SIZE, TILE_SIZE, TILE_SIZE, 0xff0000ff, render_target_id: RENDER_TARGET_ID) }
-    @exits.each { |x| Window.draw_rect(x.col * TILE_SIZE, x.row * TILE_SIZE, TILE_SIZE, TILE_SIZE, 0xffff0000, render_target_id: RENDER_TARGET_ID) }
+    @exits.each do |e|
+      x = e.col * TILE_SIZE
+      y = e.row * TILE_SIZE
+      Window.draw_rect(x, y, TILE_SIZE, TILE_SIZE, 0xffff0000, render_target_id: RENDER_TARGET_ID)
+      $font.draw_text("#{e.dest_scene},#{e.dest_entrance}", x, y, 0xffffff)
+    end
 
     @objects.each { |o| o.draw(render_target_id: RENDER_TARGET_ID) }
 
-    $args.outputs.sprites << {
+    $args.outputs.primitives << {
       path: RENDER_TARGET_ID,
       x: 0,
       y: Window.height - DISPLAY_HEIGHT,
@@ -64,20 +71,22 @@ class EditorScene < Scene
     check_wall_tiles(col, row)
   end
 
-  def check_wall_tiles(col, row)
-    set_wall_tile(col, row)
-    set_wall_tile(col, row - 1) if row > 0
-    set_wall_tile(col + 1, row) if col < TILES_X - 1
-    set_wall_tile(col, row + 1) if row < TILES_Y - 1
-    set_wall_tile(col - 1, row) if col > 0
-  end
-
   def add_entrance(col, row)
     return if @tiles[col][row]
     return if @entrances.find { |(i, j)| i == col && j == row }
     return if @exits.find { |x| x.col == col && x.row == row }
+    return if @objects.find { |x| x.col == col && x.row == row }
 
     @entrances << [col, row]
+  end
+
+  def add_exit(col, row, dest_scene, dest_entr)
+    return if @tiles[col][row]
+    return if @entrances.find { |(i, j)| i == col && j == row }
+    return if @exits.find { |x| x.col == col && x.row == row }
+    return if @objects.find { |x| x.col == col && x.row == row }
+    
+    @exits << Exit.new(col, row, dest_scene, dest_entr)
   end
 
   def delete_at(col, row)
@@ -91,6 +100,16 @@ class EditorScene < Scene
     @exits.reject! { |x| x.col == col && x.row == row }
     @objects.reject! { |x| x.col == col && x.row == row }
   end
+  
+  private
+
+  def check_wall_tiles(col, row)
+    set_wall_tile(col, row)
+    set_wall_tile(col, row - 1) if row > 0
+    set_wall_tile(col + 1, row) if col < TILES_X - 1
+    set_wall_tile(col, row + 1) if row < TILES_Y - 1
+    set_wall_tile(col - 1, row) if col > 0
+  end
 end
 
 class Editor
@@ -99,17 +118,24 @@ class Editor
   class << self
     def init
       @scene = EditorScene.new(1)
-      font = Font.new(:font, 32)
-      @buttons = [
-        Button.new(10, 10, w: 40, h: 40, anchor: :top_right, font: font, text: '#') { @active_tool = :wall },
-        Button.new(10, 60, w: 40, h: 40, anchor: :top_right, font: font, text: '/') { @active_tool = :wall_edge },
-        Button.new(10, 110, w: 40, h: 40, anchor: :top_right, font: font, text: 'e') { @active_tool = :entrance },
-        Button.new(10, 160, w: 40, h: 40, anchor: :top_right, font: font, text: 'x') { @active_tool = :exit },
+      @exit_dest_scene = 1
+      @exit_dest_entr = 0
+      @controls = [
+        Button.new(10, 10, w: 40, h: 40, anchor: :top_right, font: $font, text: '#') { @active_tool = :wall },
+        Button.new(10, 60, w: 40, h: 40, anchor: :top_right, font: $font, text: '/') { @active_tool = :wall_edge },
+        Button.new(10, 110, w: 40, h: 40, anchor: :top_right, font: $font, text: 'e') { @active_tool = :entrance },
+        Button.new(10, 160, w: 40, h: 40, anchor: :top_right, font: $font, text: 'x') { @active_tool = :exit },
+        (lbl_dest_scene = Label.new(50, 210, $font, '1', anchor: :top_right, color: 0xffffff)),
+        Button.new(10, 210, w: 32, h: 32, anchor: :top_right, font: $font, text: '>') { @exit_dest_scene += 1; lbl_dest_scene.text = @exit_dest_scene.to_s },
+        Button.new(90, 210, w: 32, h: 32, anchor: :top_right, font: $font, text: '<') { if @exit_dest_scene > 1; @exit_dest_scene -= 1; lbl_dest_scene.text = @exit_dest_scene.to_s; end },
+        (lbl_dest_entr = Label.new(50, 252, $font, '0', anchor: :top_right, color: 0xffffff)),
+        Button.new(10, 252, w: 32, h: 32, anchor: :top_right, font: $font, text: '>') { @exit_dest_entr += 1; lbl_dest_entr.text = @exit_dest_entr.to_s },
+        Button.new(90, 252, w: 32, h: 32, anchor: :top_right, font: $font, text: '<') { if @exit_dest_entr > 0; @exit_dest_entr -= 1; lbl_dest_entr.text = @exit_dest_entr.to_s; end },
       ]
     end
 
     def update
-      @buttons.each(&:update)
+      @controls.each(&:update)
       return unless Mouse.over?(0, 0, EditorScene::DISPLAY_WIDTH, EditorScene::DISPLAY_HEIGHT)
 
       col = (Mouse.x / (TILE_SIZE * SCENE_SCALE)).floor
@@ -122,6 +148,8 @@ class Editor
           @scene.add_wall_edge(col, row)
         when :entrance
           @scene.add_entrance(col, row)
+        when :exit
+          @scene.add_exit(col, row, @exit_dest_scene, @exit_dest_entr)
         end
       elsif Mouse.button_down?(:right)
         @scene.delete_at(col, row)
@@ -131,7 +159,7 @@ class Editor
     def draw
       $args.outputs.solids << { x: 0, y: 0, w: EDITOR_SCREEN_WIDTH, h: EDITOR_SCREEN_HEIGHT, r: 0, g: 0, b: 0, a: 255 }
       @scene.draw
-      @buttons.each(&:draw)
+      @controls.each(&:draw)
     end
   end
 end
