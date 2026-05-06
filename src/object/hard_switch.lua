0e8dcfe5fd40bd308ad06b3c2e8aeb838ab0d8cd
Victor David Santos <victordavidsantos@gmail.com>
2026-05-05 21:49:33 -0300

    Update save pad and switches.

--- src/object/hard_switch.lua
+++ src/object/hard_switch.lua
@@ -2,7 +2,7 @@ HardSwitch = setmetatable({}, Trigger)
 HardSwitch.__index = HardSwitch
 
 function HardSwitch.new(id, col, row, args)
-  local self = Trigger.new(id, col, row, args, 4, 4, 2, 2, "object/hard_switch", Vector.new(-3, -3), 2, 1)
+  local self = Trigger.new(id, col, row, args, 7, 7, 2, 2, "object/hard_switch" .. (args[2] or "1"), Vector.new(-5, -5), 2, 1)
   setmetatable(self, HardSwitch)
   self.serializable_attrs = {"img_index", "active"}
   return self
--- src/object/switch.lua
+++ src/object/switch.lua
@@ -2,7 +2,7 @@ Switch = setmetatable({}, Trigger)
 Switch.__index = Switch
 
 function Switch.new(id, col, row, args)
-  local self = Trigger.new(id, col, row, args, 4, 4, 2, 2, "object/switch", Vector.new(-3, -3), 2, 1)
+  local self = Trigger.new(id, col, row, args, 7, 7, 2, 2, "object/switch" .. (args[2] or "1"), Vector.new(-5, -5), 2, 1)
   setmetatable(self, Switch)
   self.serializable_attrs = {"img_index", "active"}
   return self
